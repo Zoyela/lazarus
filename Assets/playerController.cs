b3f983fedc2ebf59ad62cb592d59f07c2a0f2344
Zoyela <Tanja.kuery@students.unibe.ch>
2018-05-18 09:54:19 +0200

    first tries

---
+++ Assets/playerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour{
+
+	[SerializeField]
+	private float speed;
+
+	void Update(){
+		var x = Input.GetAxis("Horizontal") * Time.deltaTime * speed;
+		var z = Input.GetAxis("Vertical") * Time.deltaTime * speed;
+
+		transform.Rotate(0, x, 0);
+		transform.Translate(0, 0, z);
+	}
+}
